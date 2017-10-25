2957332f5c3807fd08a81038d55948fc12ac592d
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-25 01:53:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,6 +19,6 @@ def assign_rooms(array)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  puts batch_badge_creator(attendees).chomp
+  puts assign_rooms(attendees).chomp
 end
